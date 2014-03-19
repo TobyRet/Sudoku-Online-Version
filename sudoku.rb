3db3a0f48e4b38f6d67979ae3da5d2a6294824be
Toby Retallick <toby.retallick@gmail.com>
2014-03-19 11:15:05 +0000

    Fixed bug: HTML form now in right place. INdentation corrected

--- sudoku.rb
+++ sudoku.rb
@@ -13,13 +13,43 @@ end
 
 # this session removes some digits from the solution to create a puzzle
 def puzzle(sudoku)
-  20.times { sudoku[rand(0..80)] = nil }
+  puzzle = 20.times { sudoku[rand(0..80)] = "0" }
+  puzzle
+end
+
+def box_order_to_row_order(cells)
+  boxes = cells.each_slice(9).to_a
+  (0..8).to_a.inject([]) {|memo, i|
+    first_box_index = i / 3 * 3
+    three_boxes = boxes[first_box_index, 3] # What does this mean.
+    three_rows_of_three = three_boxes.map do |box|
+      row_number_in_a_box = i % 3
+      first_cell_in_the_row_index = row_number_in_a_box * 3
+      box[first_cell_in_the_row_index, 3] 
+    end
+    memo += three_rows_of_three.flatten
+  }
+end
+
+def generate_new_puzzle_if_necessary
+  return if session[:current_solution]
+  sudoku = random_sudoku
+  session[:solution] = sudoku
+  session[:puzzle] = puzzle(sudoku)
+  session[:current_solution] = session[:puzzle]
+end
+
+def prepare_to_check_solution
+  @check_solution = session[:check_solution]
+  session[:check_solution] = nil
 end
 
 get '/' do
-  sudoku = random_sudoku
-  session[:solution] = sudoku
-  @current_solution = puzzle(sudoku)
+  prepare_to_check_solution
+  generate_new_puzzle_if_necessary
+  @current_solution = session[:current_solution] || session[:puzzle]
+  @solution = session[:solution]
+  @puzzle = session[:puzzle]
   erb :index
 end
 
@@ -28,6 +58,29 @@ get '/solution' do
   erb :index
 end
 
-get'/test' do
-  erb :test
+post '/' do
+  cells = params["cell"] 
+  session[:current_solution] = cells.map{|value| value.to_i }.join
+  session[:check_solution] = true
+  redirect to("/")
+end
+
+helpers do
+
+  def colour_class(solution_to_check, puzzle_value, current_solution_value, solution_value)
+    must_be_guessed = puzzle_value == 0
+    tried_to_guess = current_solution_value.to_i != 0
+    guessed_incorrectly = current_solution_value != solution_value
+
+    if solution_to_check && must_be_guessed && tried_to_guess && guessed_incorrectly
+      'incorrect'
+    elsif !must_be_guessed
+      'value-provided'
+    end
+  end
+
+  def cell_value(value)
+    value.to_i == 0 ? '' : value
+  end
+
 end
